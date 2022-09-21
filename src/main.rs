b4ce196a644503a084976ee62967a789a4b1892a
obinna <biostechnologyng@gmail.com>
2022-09-22 00:38:11 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
 use std::io;
 fn main() {
     let mut input = String::new();
-
-    
-    //passing mutable reference
-    some_fn(&mut input);
-
-    //two imutable references
-    // let input1 = &input;
-    // let input2 = &input;
-
-    // println!("{}, {}", input1, input2);
-
     io::stdin().read_line(&mut input);
-    println!("Input: {}", input);
+
+    //points to a reference
+    borrow_string(&input);
+
+    //takes ownership of the string
+    own_string(input);
+   
     let mut mars_weight = calculate_weight_on_mars(100.00);
      mars_weight = mars_weight * 1000.0;
     println!("Weight on mars: {}kg", mars_weight);
+}
+
+fn borrow_string(s: &String){
+    println!("{}", s)
+}
+fn own_string(s: String){
+    println!("{}", s)
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight /9.81)* 3.711
 }
 
-fn some_fn(s: &mut String) {
 
-}
 //mutably borrow or immutably borrow 
 //add ampersand (&) to the variable type to make it point to a reference without taking ownership e.g s: &String
 //in rust passing references as parameters is called borrowing
