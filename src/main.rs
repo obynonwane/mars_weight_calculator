142ad9184e084f4e4d070fa3020f367c775e10b3
obinna <biostechnologyng@gmail.com>
2022-09-21 16:36:45 +0100

    First Rust Init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+fn main() {
+    let mut input = String::new();
+    some_fn(input);
+    io::stdin().read_line(&mut input);
+    let mut mars_weight = calculate_weight_on_mars(100.00);
+     mars_weight = mars_weight * 1000.0;
+    println!("Weight on mars: {}kg", mars_weight);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight /9.81)* 3.711
+}
+
+fn some_fn(s:String) {
+
+}
