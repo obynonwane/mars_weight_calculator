34fb3155aed7a804d00e42fc7a124d6b3d51f12a
obinna <biostechnologyng@gmail.com>
2022-09-21 16:40:58 +0100

    Understanding reference type and mutable reference types

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 fn main() {
     let mut input = String::new();
-    some_fn(input);
+    some_fn(&mut input);
     io::stdin().read_line(&mut input);
     let mut mars_weight = calculate_weight_on_mars(100.00);
      mars_weight = mars_weight * 1000.0;
@@ -12,6 +12,6 @@ fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight /9.81)* 3.711
 }
 
-fn some_fn(s:String) {
+fn some_fn(s: &mut String) {
 
 }
